0d9a5caea0a5f2bd8d11dc831f1bb72835c2ac1b
CHAITANYA <57499706+chaitanya392@users.noreply.github.com>
2023-12-01 23:27:22 -0500

    frontend

---
+++ FrontEnd/carrental/src/app/components/registeruser/registeruser.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-registeruser',
+  templateUrl: './registeruser.component.html',
+  styleUrls: ['./registeruser.component.scss']
+})
+export class RegisteruserComponent {
+
+}
