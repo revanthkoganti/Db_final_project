4b12203ca7bcd1c07d8906584e9b3663ab0d94b9
manideepika <manideepikaadusumilli36@gmail.com>
2023-12-02 00:04:56 -0500

    changes

--- FrontEnd/carrental/src/app/app.module.ts
+++ FrontEnd/carrental/src/app/app.module.ts
@@ -6,7 +6,10 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { RegisteruserComponent } from './components/registeruser/registeruser.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +19,11 @@ import { RegisteruserComponent } from './components/registeruser/registeruser.co
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatToolbarModule, 
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- FrontEnd/carrental/src/app/components/home/home.component.ts
+++ FrontEnd/carrental/src/app/components/home/home.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-
+  cars= [
+    {
+      id: 1,
+      brand: 'Toyota',
+      model: 'Camry',
+      year: 2022,
+      rentalPrice: 50,
+      imageUrl: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.cars.com%2Fresearch%2Ftoyota-camry-2021%2F&psig=AOvVaw1pIEekqvj5GTJQqjN44dmH&ust=1701579228789000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCMiz1d3674IDFQAAAAAdAAAAABAE', // Update with the actual path to your images
+    },
+    {
+      id: 2,
+      brand: 'Honda',
+      model: 'Accord',
+      year: 2021,
+      rentalPrice: 55,
+      imageUrl: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.cars.com%2Fresearch%2Ftoyota-camry-2021%2F&psig=AOvVaw1pIEekqvj5GTJQqjN44dmH&ust=1701579228789000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCMiz1d3674IDFQAAAAAdAAAAABAE',
+    },
+    {
+      id: 3,
+      brand: 'Ford',
+      model: 'Fusion',
+      year: 2023,
+      rentalPrice: 60,
+      imageUrl: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.cars.com%2Fresearch%2Ftoyota-camry-2021%2F&psig=AOvVaw1pIEekqvj5GTJQqjN44dmH&ust=1701579228789000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCMiz1d3674IDFQAAAAAdAAAAABAE',
+    },
+    
+  ];
 }
